38f2902db45cde1999f37f6cd04b7c70809f7215
yuemian <1272698173@qq.com>
2019-11-23 16:24:11 +0800

    -

--- src/myHooks/useStudentsByPage.js
+++ src/myHooks/useStudentsByPage.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 import { getStudentsByPage } from '../services/student';
 
+// export default function useStudentsByPage (page = 1, limit = 10) {
+//     const [resp, setResp] = useState({});
+//     useEffect(_ => {
+//         (async function () {
+//             console.log(page, limit);
+//             const resp = await getAllStudents(page, limit);
+//             // setResp(resp);
+//         })()
+//     }, [page, limit]);
+//     return resp;
+// }
+
+function reducer (state, action) {
+    return action.payload;
+}
+
 export default function useStudentsByPage (page = 1, limit = 10) {
-    const [resp, setResp] = useState({});
+    const [resp, dispatch] = useReducer(reducer, {});
     useEffect(_ => {
         (async function () {
-            const resp = await getStudentsByPage(page, limit);
-            setResp(resp);
+            const data = await getStudentsByPage(page, limit);
+            dispatch({type: "update", payload: data});
         })()
     }, [page, limit]);
     return resp;
